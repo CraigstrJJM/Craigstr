f1830643c10f7d3533c0178b56f1cb82bcb5c59b
JohnBuonomo <john.a.buonomo@gmail.com>
2014-11-10 10:41:11 -0500

    Add user authentication using monban

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :users, only: [:none] do
+  end
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
+    root "users#new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
