6ab0c110851c53d12eae5f885d806ed8cae6d28c
MTD <mtdowd@gmail.com>
2014-11-12 15:28:25 -0500

    Additional formatting fixes (adding spaces, deleting extra lines, etc.)

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,5 @@
 class SearchesController < ApplicationController
   def index
-    # @post = TextShout.where("body ILIKE ?", "%#{params[:query]}%")
-    # @shouts = Shout.where(content_type: "T", id: @text_shouts.pluck(:id))
-    # @users = User.where("email ILIKE ?", "%#{params[:query]}%")
+
   end
 end
