4026f4cba1b0ae432bbe76f067d0290af6be33de
MTD <mtdowd@gmail.com>
2014-11-12 17:01:51 -0500

    Adjusted locations_controller to only use one instance variable
    * Updated location model to include method for categories

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,4 +4,8 @@ class Location < ActiveRecord::Base
   validates :name, presence: true
   validates :state, presence: true
   validates_uniqueness_of :name, scope: :state
+
+  def categories(*args)
+    Category.all
+  end
 end
