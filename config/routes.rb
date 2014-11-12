09e685adf2974a22afdd4acadcf6bece40f72f8c
MTD <mtdowd@gmail.com>
2014-11-12 14:51:57 -0500

    Fixing formatting errors identified by Hound

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ require "monban/constraints/signed_in"
 require "monban/constraints/signed_out"
 
 Rails.application.routes.draw do
-
   constraints Monban::Constraints::SignedIn.new do
     root "dashboards#show", as: :dashboard
   end
