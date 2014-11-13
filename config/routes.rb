047f1599abe2b33ad102e946c59a1ad41768f6aa
MTD <mtdowd@gmail.com>
2014-11-13 09:53:11 -0500

    Deleted post index route from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,5 @@ Rails.application.routes.draw do
 
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
-  resources :posts, only: [:edit, :index, :new, :create, :destroy, :show]
+  resources :posts, only: [:edit, :new, :create, :destroy, :show]
 end
