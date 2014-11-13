d5097311fddcfd9f0207071d3fa6babdb4e965f1
MTD <mtdowd@gmail.com>
2014-11-13 09:45:52 -0500

    Removed dependency in category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ActiveRecord::Base
   has_many :posts
   validates :name, presence: true, uniqueness: true
 
-  def new_post
-    Post.new
+  def new_post(klass = Post)
+    klass.new
   end
 end
