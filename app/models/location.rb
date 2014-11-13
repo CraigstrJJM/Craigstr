32ebc67d0acdc053aa7daefb86b3010821d99133
MTD <mtdowd@gmail.com>
2014-11-13 11:45:21 -0500

    Removed dependency in location model
    * Adjusted locations show view to display Category.all

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,8 +4,4 @@ class Location < ActiveRecord::Base
   validates :name, presence: true
   validates :state, presence: true
   validates_uniqueness_of :name, scope: :state
-
-  def list_categories(klass = Category)
-    klass.all
-  end
 end
